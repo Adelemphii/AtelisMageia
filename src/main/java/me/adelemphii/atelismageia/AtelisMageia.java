3d4af24ccd28384c7220a6585d3969002f95c8f5
Adelemphii <demilitioowner@gmail.com>
2021-11-15 21:08:29 -0600

    Initial commit

---
+++ src/main/java/me/adelemphii/atelismageia/AtelisMageia.java
@@ -0,0 +1,16 @@
+package me.adelemphii.atelismageia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AtelisMageia extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
